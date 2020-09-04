45ffcbc804a0706831169256588a4f5dd932efeb
Nikolai Sison <sisonnikolai@gmail.com>
2020-09-04 22:31:59 +0800

    Necessary project files

---
+++ AutoTempCleaner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace AutoTempCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<TempCleaner>(c =>
+                {
+                    c.ConstructUsing(cleaner => new TempCleaner());
+                    c.WhenStarted(cleaner => cleaner.Start());
+                    c.WhenStopped(cleaner => cleaner.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("AutoTempCleaner");
+                x.SetDisplayName("Auto Temp Cleaner");
+                x.SetDescription("Service app that automatically cleans the temp folder");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
---
+++ AutoTempCleaner/TempCleaner.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AutoTempCleaner
+{
+    public class TempCleaner
+    {
+        private readonly Timer timer;
+        private readonly string tempPath = ConfigurationManager.AppSettings.Get("TempFolderPath");
+
+        public TempCleaner()
+        {
+            timer = new Timer(1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            File.SetAttributes(tempPath, FileAttributes.Normal);
+
+            string[] directories = Directory.GetDirectories(tempPath);
+
+            foreach (var dir in directories)
+            {
+                var subDir = Directory.GetFiles(dir);
+
+                if (subDir.Length > 0)
+                {
+                    DeleteDirFiles(dir);
+                }
+                else
+                {
+                    Directory.Delete(dir);
+                }
+            }
+
+            string[] files = Directory.GetFiles(tempPath);
+
+            foreach (var file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+        }
+
+        private void DeleteDirFiles(string dirPath)
+        {
+            File.SetAttributes(dirPath, FileAttributes.Normal);
+
+            string[] files = Directory.GetFiles(dirPath);
+
+            foreach (var file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
